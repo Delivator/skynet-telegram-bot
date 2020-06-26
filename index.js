00dd2ced680db9a3fdfb182ab50013dd9c8c8fb0
Delivator <info@delivator.me>
2020-06-27 01:56:10 +0200

    v0.0.2 Added more message types and added ratelimit

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const { Telegraf } = require("telegraf");
 const axios = require("axios").default;
 const settings = require("./settings");
+const rateLimit = require("telegraf-ratelimit");
 
 const apiUrl = settings.portalUrl + "/skynet/skyfile";
 const bot = new Telegraf(settings.telegramBotToken);
 
-bot.start((ctx) =>
-  ctx.reply("Send me a file or text and I will upload it to SkyPortal.xyz!")
-);
-bot.help((ctx) =>
-  ctx.reply("Send me a file or text and I will upload it to SkyPortal.xyz!")
-);
-bot.on("document", (ctx) => {
+// config for telegraf ratelimit middleware
+const limitConfig = {
+  window: settings.rateLimitTime,
+  limit: 1,
+  onLimitExceeded: (ctx) =>
+    ctx.reply(
+      `Rate limit exceeded. Max 1 upload per ${settings.rateLimitTime / 1000}s`
+    ),
+};
+
+async function uploadFile(fileId, filename, ctx) {
+  let reply = await ctx.reply(`Downloading file...`, {
+    reply_to_message_id: ctx.message.message_id,
+  });
   ctx.telegram
-    .getFileLink(ctx.message.document.file_id)
-    .then(async (url) => {
-      console.log(`Downloading ${url}`);
-      let reply = await ctx.reply(`Downloading file...`, {
-        reply_to_message_id: ctx.message.message_id,
-      });
+    .getFileLink(fileId)
+    .then((url) => {
       axios
         .get(url, { responseType: "arraybuffer" })
         .then((response) => {
@@ -29,10 +33,9 @@ bot.on("document", (ctx) => {
             "Uploading to Skynet..."
           );
           axios
-            .post(
-              apiUrl + "?filename=" + ctx.message.document.file_name,
-              response.data
-            )
+            .post(apiUrl + "?filename=" + filename, response.data, {
+              maxContentLength: Infinity,
+            })
             .then((resp) => {
               ctx.telegram.editMessageText(
                 ctx.chat.id,
@@ -47,5 +50,55 @@ bot.on("document", (ctx) => {
         .catch(console.error);
     })
     .catch(console.error);
+}
+
+async function uploadText(ctx) {
+  let filename = `text_${new Date().getTime()}.txt`;
+  let reply = await ctx.reply(`Uploading text...`, {
+    reply_to_message_id: ctx.message.message_id,
+  });
+  axios
+    .post(apiUrl + "?filename=" + filename, ctx.message.text, {
+      maxContentLength: Infinity,
+      // headers: { "content-type": "plain/text" },
+    })
+    .then((resp) => {
+      ctx.telegram.editMessageText(
+        ctx.chat.id,
+        reply.message_id,
+        null,
+        `${settings.portalUrl}/${resp.data.skylink}`,
+        { disable_web_page_preview: true }
+      );
+    })
+    .catch(console.error);
+}
+
+bot.start((ctx) => ctx.reply(settings.helpMsg));
+
+bot.use(rateLimit(limitConfig));
+
+bot.help((ctx) => ctx.reply(settings.helpMsg));
+
+bot.on("document", async (ctx) => {
+  uploadFile(ctx.message.document.file_id, ctx.message.document.file_name, ctx);
 });
+
+bot.on("photo", (ctx) => {
+  let file = ctx.message.photo.slice(-1).pop().file_id;
+  uploadFile(file, `photo_${ctx.message.date}.jpg`, ctx);
+});
+
+bot.on("voice", (ctx) => {
+  uploadFile(ctx.message.voice.file_id, `audio_${ctx.message.date}.ogg`, ctx);
+});
+
+bot.on("video", (ctx) => {
+  uploadFile(ctx.message.video.file_id, `video_${ctx.message.date}.mp4`, ctx);
+});
+
+bot.on("text", (ctx) => {
+  uploadText(ctx);
+});
+
 bot.launch();
--- settings-example.js
+++ settings-example.js
@@ -1,4 +1,6 @@
 module.exports = {
   telegramBotToken: "012345678:abcdefghijklmnopqrstuvwxyzABCDEFGHI",
   portalUrl: "https://skyportal.xyz",
+  helpMsg: "Send me a file or text and I will upload it to SkyPortal.xyz!",
+  rateLimitTime: 10000,
 };
